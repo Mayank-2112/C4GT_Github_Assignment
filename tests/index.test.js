a7585af1c508a149d5b3d2beed3f932459495990
Mayank2112 <30808mayank21@gmail.com>
2024-04-25 12:30:43 +0530

    Task 3 Completed

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,4 +1,11 @@
 const { readCSV } = require("../src/csvReader.js");
+const { queryParser } = require("../src/queryParser.js");
+
+test('Basic Jest test',()=>{
+    expect(1).toBe(1);
+});
+
+
 
 test('Read CSV File',async ()=>{
     const data = await readCSV('sample.csv');
@@ -8,3 +15,11 @@ test('Read CSV File',async ()=>{
     expect(data[0].age).toBe('30');
 });
 
+test('Parse Sql Query', ()=>{
+    const query = 'SELECT id, name FROM sample';
+    const parsed = queryParser(query);
+    expect(parsed).toEqual({
+        fields: ['id','name'],
+        table: 'sample'
+    });
+});
